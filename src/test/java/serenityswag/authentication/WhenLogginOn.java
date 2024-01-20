a2798250363d4badb82a21c1712ca0562d700a25
Mancilla <mancillavelazquez@gmail.com>
2024-01-20 22:38:49 +0100

    adding new class

---
+++ src/test/java/serenityswag/authentication/WhenLogginOn.java
@@ -0,0 +1,9 @@
+package serenityswag.authentication;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(SerenityRunner.class)
+public class WhenLogginOn {
+
+}
